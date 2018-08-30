c99318aa05bc2e246a8dee7603c58c9620a6508a
tia-s-edwards <42820887+tia-s-edwards@users.noreply.github.com>
2018-08-29 22:50:40 -0400

    Add files via upload

---
+++ Fall.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { StyleSheet, ImageBackground, Image, View, PanResponder, Animated, Dimensions } from 'react-native';
+
+
+const { width } = Dimensions.get("window")
+
+export default class App extends React.Component {
+     render(){
+        return(
+            <View></View>
+
+        )
+
+     }
+
+}
